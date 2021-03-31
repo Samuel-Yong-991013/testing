2b9a4fce5d7708331fef1fbe4b808514eb211952
Samuel Yong Syn Wei <samuelyong91@gmail.com>
2021-03-31 11:38:10 +0800

    testing out 'git cherry-pick [commit ish] -n'

--- file1.js
+++ file1.js
@@ -3,7 +3,11 @@ consectetur adipiscing elit,
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Exc
 
+<<<<<<< HEAD
 awefauibwflu
 
 awrgawrg
 awrgarwgagagrknlinknlkjnzdvzxcv
+=======
+naflieufnawiefni
+>>>>>>> 81b13af (messed up file1.js)
